c00e0a969fcb1fcf8aa91faf006a6989beb96c57
n23space <n23space@gmail.com>
2017-07-02 12:01:51 +0530

    set listening port to process.env.PORT || 8081

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.post('/download*', function (req, res) {
   res.download(file, fileName + '.xlsx'); // Set disposition and send it.
 });
 
-var server = app.listen(8081, function () {
+var server = app.listen(process.env.PORT || 8081, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log(`Example app listening at http://${host}:${port}`);
